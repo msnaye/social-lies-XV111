543107baa51c64d6edf00b156743db0c0e6f7d8b
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-24 22:03:15 -0500

    modified and added to server.js

--- server.js
+++ server.js
@@ -9,4 +9,12 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(require('./routes'));
 
+mongoose.connect(process.env.MONGODB_URI || 'mongdb//localhost',{
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
 app.listen(PORT, ()=> console.log())
